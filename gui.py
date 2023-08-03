f1c9aadac4c9e4699ac95deb4b21ea7cb637f2d8
Egeland91 <120024040+egeland00@users.noreply.github.com>
2023-08-03 13:08:13 +0100

    Refactor and comments
    Refactoring the login gui code to use classes. Also did new comments

--- gui.py
+++ gui.py
@@ -1,3 +1,4 @@
+# Importing required libraries
 import tkinter as tk
 from tkinter import *
 from tkinter import ttk
@@ -5,53 +6,59 @@ from tkinter import messagebox
 import ttkbootstrap as ttkbs
 from ttkbootstrap import *
 
+# Defining the GUI class for the application
+class PhishingDetectorGUI:
+    def __init__(self):
+        # Creating the main application window with a title and fixed geometry.
+        self.root = ttkbs.Window(themename="superhero")
+        self.root.title("Phishing Detection")
+        self.root.geometry("1920x1080")
 
-#Making window using ttkbootstrap (theme = superhero)
-root = ttkbs.Window(themename="superhero")
-root.title("Phishing Detection")
-root.geometry("1920x1080")
+        # Creating a Frame to hold the widgets in the center of the window
+        self.frame = ttkbs.Frame(self.root)
+        self.frame.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
 
-#Creating a Frame to hold the widgets in center of window
-frame = ttkbs.Frame(root)
-frame.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
+        # Adding a header label to the GUI
+        self.header = ttkbs.Label(self.frame, text="Welcome to Phishing Detection(EDUCATIONAL PURPOSE ONLY)", style="info", font=("Ubuntu", 24, "bold"))
+        self.header.grid(column=0, row=0, padx=5, pady=10)
 
-# Add a header label
-header = ttkbs.Label(frame, text="Welcome to Phishing Detection(EDUCATIONAL PURPOSE ONLY)", style="info", font=("Ubuntu", 24, "bold"))
-header.grid(column=0, row=0, padx=5, pady=10)
+        # Adding a checkbutton for terms of agreement and linking it to the 'show_terms' function
+        self.checkbutton = ttkbs.Checkbutton(self.frame, text="Terms of Agreement", style="secondary", command=self.show_terms)
+        self.checkbutton.grid(column=0, row=8, padx=5, pady=10)
 
-#Terms of Agreement button and function to show terms
-def show_terms():
-    with open('/home/orjan/Documents/GitHub/masters-project/TOA.txt', 'r') as f:
-        terms = f.read()
-    messagebox.showinfo("Terms of Agreement", terms)
+        # Adding labels and entry fields for Email and Password
+        self.email_label = ttkbs.Label(self.frame, text="Email", style="info")
+        self.email_label.grid(column=0, row=1, padx=5, pady=10)
+        self.email_entry = ttkbs.Entry(self.frame, width=30, style="info")
+        self.email_entry.grid(column=0, row=2, padx=5, pady=10)
+        
+        self.password_label = ttkbs.Label(self.frame, text="Password", style="info")
+        self.password_label.grid(column=0, row=3, padx=5, pady=10)
+        self.password_entry = ttkbs.Entry(self.frame, width=30, style="info")
+        self.password_entry.grid(column=0, row=4, padx=5, pady=10)
 
-#Terms of Agreement button and function to show terms
-checkbutton = ttkbs.Checkbutton(frame, text="Terms of Agreement", style="secondary", command=show_terms)
-checkbutton.grid(column=0, row=8, padx=5, pady=10)
+        # Adding a combobox for choosing the email client
+        self.server_label = ttkbs.Label(self.frame, text="Choose Email Client", style="info")
+        self.server_label.grid(column=0, row=5, padx=5, pady=10)
+        self.server_combobox = ttkbs.Combobox(self.frame, width=27, style="info")
+        self.server_combobox['values'] = ('Outlook (Working)', 'Gmail (Not Working)', 'Others (Not Working)')
+        self.server_combobox.grid(column=0, row=6, padx=5, pady=10)
 
-# Email and password entry labels and entry fields
-email_label = ttkbs.Label(frame, text="Email", style="info")
-email_label.grid(column=0, row=1, padx=5, pady=10)
-email_entry = ttkbs.Entry(frame, width=30, style="info")
-email_entry.grid(column=0, row=2, padx=5, pady=10)
+        # Adding a Login button
+        self.loginbutton = ttkbs.Button(self.frame, text="Login", bootstyle='success')
+        self.loginbutton.grid(column=0, row=7, pady=10)
 
-password_label = ttkbs.Label(frame, text="Password", style="info")
-password_label.grid(column=0, row=3, padx=5, pady=10)
-password_entry = ttkbs.Entry(frame, width=30, style="info")
-password_entry.grid(column=0, row=4, padx=5, pady=10)
+    # Function to read and display the terms of agreement
+    def show_terms(self):
+        with open('/home/orjan/Documents/GitHub/masters-project/TOA.txt', 'r') as f:
+            terms = f.read()
+        messagebox.showinfo("Terms of Agreement", terms)
 
-#Choose imap server using combobox
-server_label = ttkbs.Label(frame, text="Choose Email Client", style="info")
-server_label.grid(column=0, row=5, padx=5, pady=10)
-server_combobox = ttkbs.Combobox(frame, width=27, style="info")
-server_combobox['values'] = ('Outlook (Working)', 'Gmail (Not Working)', 'Others (Not Working)')
-server_combobox.grid(column=0, row=6, padx=5, pady=10)
+    # Function to start the GUI
+    def run(self):
+        self.root.mainloop()
 
-
-
-#Login button
-loginbutton = ttkbs.Button(frame, text="Login", bootstyle=SUCCESS)
-loginbutton.grid(column=0, row=7, pady=10)
-
-root.mainloop()
-
+# Creating an instance of the GUI and running it
+if __name__ == "__main__":
+    app = PhishingDetectorGUI()
+    app.run()
