8ed2ad284a82246e8bb42bb5300b5d54784dc716
Egeland91 <120024040+egeland00@users.noreply.github.com>
2023-08-03 13:56:08 +0100

    Imap and cleanup
    Added Imap function to be able to login to email client. Aslo cleaned up a bit of imports that i didnt use

--- gui.py
+++ gui.py
@@ -1,10 +1,10 @@
 # Importing required libraries
-import tkinter as tk
 from tkinter import *
-from tkinter import ttk
+import tkinter as tk
 from tkinter import messagebox
+from ttkbootstrap.constants import *
 import ttkbootstrap as ttkbs
-from ttkbootstrap import *
+from email_fetcher import EmailFetcher
 
 # Defining the GUI class for the application
 class PhishingDetectorGUI:
@@ -19,11 +19,11 @@ class PhishingDetectorGUI:
         self.frame.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
 
         # Adding a header label to the GUI
-        self.header = ttkbs.Label(self.frame, text="Welcome to Phishing Detection(EDUCATIONAL PURPOSE ONLY)", style="info", font=("Ubuntu", 24, "bold"))
+        self.header = ttkbs.Label(self.frame, text="Welcome to Phishing Detection(EDUCATIONAL PURPOSE ONLY)", bootstyle="default", font=("Ubuntu", 24, "bold"))
         self.header.grid(column=0, row=0, padx=5, pady=10)
 
         # Adding a checkbutton for terms of agreement and linking it to the 'show_terms' function
-        self.checkbutton = ttkbs.Checkbutton(self.frame, text="Terms of Agreement", style="secondary", command=self.show_terms)
+        self.checkbutton = ttkbs.Checkbutton(self.frame, text="Terms of Agreement", style="info", command=self.show_terms)
         self.checkbutton.grid(column=0, row=8, padx=5, pady=10)
 
         # Adding labels and entry fields for Email and Password
@@ -44,9 +44,25 @@ class PhishingDetectorGUI:
         self.server_combobox['values'] = ('Outlook (Working)', 'Gmail (Not Working)', 'Others (Not Working)')
         self.server_combobox.grid(column=0, row=6, padx=5, pady=10)
 
-        # Adding a Login button
-        self.loginbutton = ttkbs.Button(self.frame, text="Login", bootstyle='success')
+        # Adding a Login button and linking it to the 'login' function
+        self.loginbutton = ttkbs.Button(self.frame, text="Login", bootstyle='success', command=self.login)
         self.loginbutton.grid(column=0, row=7, pady=10)
+
+        # Function to handle login
+    def login(self):
+        # Get email, password and email service from the fields
+        email = self.email_entry.get()
+        password = self.password_entry.get()
+        email_service = self.server_combobox.get()
+
+        # Create EmailFetcher instance
+        email_fetcher = EmailFetcher(email, password, email_service)
+        
+        # Call login method of the email fetcher and show a success/error message based on its result
+        if email_fetcher.login():
+            messagebox.showinfo("Success", "Logged in successfully!")
+        else:
+            messagebox.showerror("Error", "Failed to log in!")
 
     # Function to read and display the terms of agreement
     def show_terms(self):
