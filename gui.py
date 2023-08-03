64de494244c012681f6db63a23e9a06383b8cc56
Egeland91 <120024040+egeland00@users.noreply.github.com>
2023-08-03 10:28:08 +0100

    TOA file added
    Added AI generated TOA as txt file with messagebox popup. Changed colour for the checkbox

--- gui.py
+++ gui.py
@@ -1,16 +1,28 @@
 import tkinter as tk
 from tkinter import *
 from tkinter import ttk
+from tkinter import messagebox
 import ttkbootstrap as ttkbs
 from ttkbootstrap import *
 
 
+#Making window using ttkbootstrap (theme = superhero)
 root = ttkbs.Window(themename="superhero")
 
+#Login button
 loginbutton = ttkbs.Button(root, text="Login", bootstyle=SUCCESS)
 loginbutton.pack(side=LEFT, padx=5, pady=10)
 
-checkbutton = ttkbs.Checkbutton(root, text="Terms of Agreement", style="success")
-checkbutton.pack(side=LEFT, padx=5, pady=10)
+#Terms of Agreement button and fucntion to show terms
+def show_terms():
+    with open('/home/orjan/Documents/GitHub/masters-project/TOA.txt', 'r') as f:
+        terms = f.read()
+    messagebox.showinfo("Terms of Agreement", terms)
+
+#Terms of Agreement button and function to show terms
+checkbutton = ttkbs.Checkbutton(root, text="Terms of Agreement", style="secondary", command=show_terms)
+checkbutton.pack(side=tk.LEFT, padx=5, pady=10)
+
+
 
 root.mainloop()
