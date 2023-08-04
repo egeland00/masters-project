cb68b3d512992e54b1a77c16d869a882608b95e8
Egeland91 <120024040+egeland00@users.noreply.github.com>
2023-08-04 22:44:18 +0100

    update email_fetcher.py
    merge added duplicate, removed

--- email_fetcher.py
+++ email_fetcher.py
@@ -39,54 +39,6 @@ class EmailFetcher:
             return True  # Return True if login was successful
         except imaplib.IMAP4.error:
             return False  # Return False if login failed
-
-    
-    # Method for logging out of the user's email session
-    def logout(self):
-        try:
-            # Logout from the IMAP session
-            self.mail.logout()
-            return True
-        except Exception as e:
-            print(f"Logout failed: {e}")
-            return False
-    # Load all emails from the user's email account. Returns a list of dictionaries, where each dictionary represents an email.(Typehint)
-    def load_all_emails(self) -> List[Dict[str, Union[str, bool]]]:
-        # The IMAP `select` command is used to select the "inbox" mailbox. This is the mailbox from which the emails will be fetched.
-        self.mail.select("inbox")
-        
-        # The IMAP `search` command is used to find all emails in the selected mailbox. The `None` argument means that no specific criteria are used for the search, so all emails will be returned.
-        _, data = self.mail.search(None, "ALL")
-        
-        # The `_fetch_emails` method is called with the email IDs returned by the `search` command. It fetches the emails from the server and returns them as a list of dictionaries.
-        return self._fetch_emails(data)
-
-    def _fetch_emails(self, data):
-        # The input `data` contains a list of email IDs. The first item of the list is split into individual email IDs.
-        email_ids = data[0].split()
-
-        # An empty list `emails` is initialised to store the fetched emails.
-        emails = []
-
-        # For each email ID in the list of email IDs...
-        for email_id in email_ids:
-            # the IMAP `fetch` command is used to fetch the email with the given ID. The `BODY.PEEK[]` argument means that the email is fetched without marking it as read.
-            _, data = self.mail.fetch(email_id, "(BODY.PEEK[])")
-            
-            # The raw email data is parsed into an `EmailMessage` object.
-            email_message: EmailMessage = email.message_from_bytes(data[0][1])
-            
-            # The `_parse_email` method is called with the `EmailMessage` object to convert the email into a more usable dictionary format.
-            email_dict = self._parse_email(email_message)
-            
-            # The dictionary representing the email is appended to the list of emails.
-            emails.append(email_dict)
-
-        # The list of emails is returned.
-        return emails
-
-    
-
     
     # Method for logging out of the user's email session
     def logout(self):
