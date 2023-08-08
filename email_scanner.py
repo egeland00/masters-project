9969bd5aa1de50b1076fa7e3f400e10e7a87c684
Egeland91 <120024040+egeland00@users.noreply.github.com>
2023-08-08 10:13:08 +0100

    adding scanner class
    Added email scanner class
    - started on different function for the ML

---
+++ email_scanner.py
@@ -0,0 +1,13 @@
+class EmailScanner:
+    def __init__(self, dataset_path) # add path to dataset
+        self.dataset = pd.read_csv(dataset_path)
+        self.preprocess_dataset()
+        self.train_model()    
+
+    def preprocess_dataset(self):
+
+
+    def train_model(self):
+
+
+    
