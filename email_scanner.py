8c2888488cde2e432989907044cf39046d9f3f42
Egeland91 <120024040+egeland00@users.noreply.github.com>
2023-08-08 12:11:33 +0100

    Importing Libraries
    import necessary libraries

--- email_scanner.py
+++ email_scanner.py
@@ -1,3 +1,11 @@
+import pandas as pd
+import nltk
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.model_selection import train_test_split
+
+
+
 class EmailScanner:
     def __init__(self, dataset_path) # add path to dataset
         self.dataset = pd.read_csv(dataset_path)
